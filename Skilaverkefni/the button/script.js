b831db0189e419e610d996515f2233cff4f31b35
alex <106684154+Figure69@users.noreply.github.com>
2025-03-21 12:13:15 +0000

    d

--- Skilaverkefni/the button/script.js
+++ Skilaverkefni/the button/script.js
@@ -1,17 +1,17 @@
 const counter = document.getElementById("h1");
 let count = 0;
 
-// Function to get the input value
+
 function getInputValue() {
     return parseInt(document.getElementById("input_id").value) || 0; 
 }
 
-// Function to get the limit value
+
 function getLimitValue() {
     return parseInt(document.getElementById("limit_id").value) || 0; 
 }
 
-// Function to load values from localStorage
+
 function loadValues() {
     const savedInput = localStorage.getItem('inputValue');
     const savedLimit = localStorage.getItem('limitValue');
@@ -25,19 +25,19 @@ function loadValues() {
     }
 }
 
-// Function to save values to localStorage
+
 function saveValues() {
     localStorage.setItem('inputValue', document.getElementById("input_id").value);
     localStorage.setItem('limitValue', document.getElementById("limit_id").value);
 }
 
-// Load values when the page loads
+
 loadValues();
 
 document.getElementById("plus").onclick = function() {
-    count += getInputValue(); // Get the input value
+    count += getInputValue(); 
     counter.innerHTML = count;
-    // Save new input value to localStorage
+   
     saveValues();
 
     if(count >= getLimitValue()) {
